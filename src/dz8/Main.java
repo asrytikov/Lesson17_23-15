16166b793dc6f8787e72ec8543363ce0710369b7
asrytikov <asrytikov@gmail.com>
2023-10-23 21:30:06 +0400

    Initial commit

---
+++ src/dz8/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i<50; i++) {
+            System.out.println(genStr((int)(Math.random()*20 + 5)));
+        }
+    }
+
+    public static String genStr(int len){
+        String res = "";
+        final char[] masChar = "ABCDabcd".toCharArray();
+        for (int i = 0; i<len; i++){
+            char t = masChar[(int)(Math.random()* masChar.length)];
+            res = res + String.valueOf(t);
+        }
+        return res;
+    }
+}
+
